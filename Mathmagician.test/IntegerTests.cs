c575add7ed05f7e0e2069f3b68998c89349346a6
alihamsa <sharif4203@hotmail.com>
2016-02-22 21:30:40 -0600

    add integers and even

---
+++ Mathmagician.test/IntegerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.test
+{
+    [TestClass]
+    public class IntergerTests
+    {
+        [TestMethod]
+        public void IntegerEnsureICanCreateAnInstance()
+        {
+            Integer my_int = new Integer();
+            Assert.IsNotNull(my_int);
+        }
+
+        [TestMethod]
+        public void IntegerEnsureIcanGetFirstNumber()
+        {
+            // Arrange -I Scenario Setup
+            Integer my_int = new Integer();
+
+            // Act - Do the thing you want to test
+            int actual = my_int.GetFirst();
+            int expected = 0;
+            // Assert - Did it work as expected?
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IntegerEnsureICanGetNextInteger()
+        {
+            //Arrange
+            Integer my_int = new Integer();
+            //Act
+            int actual = my_int.GetNext(5);
+            int expected = 6;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void IntegerEnsureICanCreateASequenceofTenIntegers()
+        {
+            //Arrange
+            Integer my_int = new Integer();
+
+            //Act
+            int[] actual = my_int.GetSequence(10);
+            int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Mathmagician/Integer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Integer
+    {
+        public int GetFirst()
+        {
+            return 0;
+        }
+
+        public int GetNext(int current)
+        {
+            return current + 1;
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            int[] seq_array = new int[how_many];
+            /*
+            for (int i = 0; i < how_many; i++)
+            {
+                seq_array[i] = i;
+            }
+            return seq_array;
+            */
+            int counter = 0;
+            seq_array[counter] = GetFirst();
+            while (counter < how_many)
+            {
+                /*
+                seq_array[counter + 1] = GetNext(counter);
+                counter++;
+                */
+            }
+        }
+    }
+}
