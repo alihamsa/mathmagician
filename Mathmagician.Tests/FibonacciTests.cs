bb4b084214f3413fffa9054fd8f152c27283785e
alihamsa <sharif4203@hotmail.com>
2016-02-29 21:38:39 -0600

    fibanacci

---
+++ Mathmagician.Tests/FibonacciTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void FibonacciEnsureICanCreateInstance()
+        {
+            Fibonacci my_Fibonacci = new Fibonacci();
+            Assert.IsNotNull(my_Fibonacci);
+        }
+
+        [TestMethod]
+        public void FibonacciEnsureICanGetFirst()
+        {
+            // Arrange
+            Fibonacci my_Fibonacci = new Fibonacci();
+
+            // Act
+            int actual = my_Fibonacci.GetFirst();
+            int expected = 1;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciEnsureICanGetNext()
+        {
+            // Arrange
+            Fibonacci my_Fibonacci = new Fibonacci();
+
+            // Act
+            int actual = my_Fibonacci.GetNext(6);
+            int expected = 8;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciIntegerEnsureICanCreateASequenceOfTen()
+        {
+            // Arrange
+            Fibonacci my_Fibonacci = new Fibonacci();
+
+            // Act
+            int[] actual = my_Fibonacci.GetSequence(10);
+            int[] expected = new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34};
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void FibonacciCalcNthFibonacciNumber()
+        {
+            // Arrange
+            Fibonacci my_Fibonaccis = new Fibonacci();
+
+            // Act
+            int actual = my_Fibonaccis.GetNth(2);
+            int expected = 1; 
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
---
+++ Mathmagician/Fibonacci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Fibonacci : Integer
+    {
+        public Fibonacci()
+        {
+            First = 1;
+        }
+        public override int GetNext(int current)
+        {
+            return base.GetNext(current);
+        }
+    }
+}
